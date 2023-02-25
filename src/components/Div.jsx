6eb56b576ca6b645a6957e742df101781e01b511
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-02-25 12:41:35 +0500

    shape creator finished

--- src/components/Div.jsx
+++ src/components/Div.jsx
@@ -1,56 +1,103 @@
 import React, { Component } from 'react';
+import "../components/div.css"
 
-class CreateComponent extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            color: '',
-            size: '',
-            width: '',
-            height: '',
-        };
+class CreateImage extends Component {
+    state = {
+        shape: 'circle',
+        color: 'red',
+        width: '100px',
+        height: '100px'
     }
 
-    changeHandler = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+    handleSelectChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
     }
 
-    handleCreate = () => {
-        let { color, size, width, height } = this.state
-        this.setState(`Color: ${color}, Size: ${size}, Width: ${width}, Height: ${height}`)
+    handleCreateButton = () => {
+        const { shape, color, width, height } = this.state;
+        return (
+            <div className='div'>
+                <div className="div-wrapper">
+                    <div>
+                        <select name="shape" onChange={this.handleSelectChange} defaultValue="circle">
+                            <option value="circle">Circle</option>
+                            <option value="triangle">Triangle</option>
+                            <option value="square">Square</option>
+                            <option value="rectangle">Rectangle</option>
+                        </select>
+                        <select name="color" onChange={this.handleSelectChange} defaultValue="red">
+                            <option value="red">Red</option>
+                            <option value="rgb(13, 245, 71)">Green</option>
+                            <option value="rgb(51, 206, 237)">Lightblue</option>
+                            <option value="pink">Pink</option>
+                        </select>
+                        <select name="width" onChange={this.handleSelectChange} defaultValue="100px">
+                            <option value="100px">100px</option>
+                            <option value="200px">200px</option>
+                            <option value="300px">300px</option>
+                            <option value="400px">400px</option>
+                        </select>
+                        <select name="height" onChange={this.handleSelectChange} defaultValue="100px">
+                            <option value="100px">100px</option>
+                            <option value="200px">200px</option>
+                            <option value="300px">300px</option>
+                            <option value="400px">400px</option>
+                        </select>
+                        <button onClick={this.handleCreateButton}>Create</button>
+                    </div>
+                    {this.renderShape(shape, color, width, height)}
+                </div>
+            </div>
+        )
+    }
+
+    renderShape = (shape, color, width, height) => {
+        if (shape === 'circle') {
+            return <div style={{
+                borderRadius: '50%',
+                backgroundColor: color,
+                width: width,
+                height: height,
+                margin: '100px auto'
+            }}>
+            </div>
+        } else if (shape === 'triangle') {
+            return <div style={{
+                width: '0',
+                height: '0',
+                borderLeft: ` ${width} solid transparent`,
+                borderRight: `${width} solid transparent`,
+                borderBottom: `${height} solid ${color}`,
+                margin: '100px auto'
+            }
+            }>
+            </div >
+        } else if (shape === 'square') {
+            return <div style={{
+                backgroundColor: color,
+                width: width,
+                height: width,
+                margin: '100px auto'
+            }}>
+            </div>
+        } else if (shape === 'rectangle') {
+            return <div style={{
+                backgroundColor: color,
+                width: width,
+                height: height,
+                margin: '100px auto'
+            }}>
+            </div>
+        }
     }
 
     render() {
         return (
             <div>
-                <select name='color' onChange={this.changeHandler}>
-                    <option value="red">Red</option>
-                    <option value="blue">Blue</option>
-                    <option value="green">Green</option>
-                    <option value="yellow">Yellow</option>
-                </select>
-                <select name='size' onChange={this.changeHandler}>
-                    <option value="small">Small</option>
-                    <option value="medium">Medium</option>
-                    <option value="large">Large</option>
-                </select>
-                <select name='width' onChange={this.changeHandler}>
-                    <option value="100">100px</option>
-                    <option value="200">200px</option>
-                    <option value="300">300px</option>
-                </select>
-                <select name='height' onChange={this.changeHandler}>
-                    <option value="100">100px</option>
-                    <option value="200">200px</option>
-                    <option value="300">300px</option>
-                </select>
-                <button onClick={this.handleCreate}>Create</button>
-            </div>
-        );
-    }
+            {this.handleCreateButton()}
+        </div>
+    );
+}
 }
 
-export default CreateComponent;
+export default CreateImage;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AddUser from './components/AddUser';
-import CreateComponent from './components/Div';
+import CreateImage from './components/Div';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
-    <CreateComponent />
+    <CreateImage/>
     <AddUser/>
   </React.StrictMode>
 );
