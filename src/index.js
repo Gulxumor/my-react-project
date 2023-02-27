6114c639713488b4e72aec9e2fcd1c0c69750634
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-02-27 16:42:48 +0500

    debugged

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AddUser from './components/AddUser.jsx';
 import ShapeCreator from './components/Div.jsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
+    <AddUser/>
     < ShapeCreator/>
   </React.StrictMode>
 );
