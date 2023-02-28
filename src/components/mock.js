f21d745b21de17b728df4ecb3cb4ecce5d115681
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-02-28 17:17:21 +0500

    Table created

---
+++ src/components/mock.js
@@ -0,0 +1,9 @@
+export const student = [
+    { id: 1, name: 'John', status: 'Frontend developer', age:20 },
+    { id: 2, name: 'Jack', status: 'Developer', age:20 },
+    { id: 3, name: 'Lily', status: 'Master', age:20 },
+    { id: 4, name: 'Boy', status: 'Teacher', age:20 },
+    { id: 5, name: 'Ali', status: 'Director', age:20 },
+    { id: 6, name: 'Girl', status: 'Mentor', age:20 },
+    { id: 7, name: 'Alice', status: 'Backend developer', age:20 },
+];
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AddUser from './components/AddUser.jsx';
-import ShapeCreator from './components/Div.jsx';
+import Table from './components/Crud.jsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
-    <AddUser/>
-    < ShapeCreator/>
+    <Table/>
   </React.StrictMode>
 );
