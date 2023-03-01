1d988e28399b86d9cce5e46cd2ae18b0a2451e67
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-01 22:23:09 +0500

    not finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import FindState from './components/homework';
-import Table from './components/Crud.jsx',
+import Table from './components/Crud.jsx'
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
