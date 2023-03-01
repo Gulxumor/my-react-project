6e230c72dd7fba373f45b78ceca6168dd3358f4a
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-01 22:13:27 +0500

    countries

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Table from './components/Crud.jsx';
+import FindState from './components/homework';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
-    <Table />
+   <FindState />
   </React.StrictMode>
 );
