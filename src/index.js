0bb17407edd1b70c72528fe58c1f10257fb83e2c
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-02 14:17:12 +0500

    cleaned from unuseful files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import FindState from './components/homework';
-import Table from './components/Crud.jsx'
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
