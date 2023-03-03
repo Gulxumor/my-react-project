5652e89743f7bcb711ac14d5104b26caf65061c9
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-03 16:53:09 +0500

    9-darss

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import States from './components/Countries';
-// import FindState from './components/homework';
 import './index.css';
+import Component from './components/styleComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
-    {/* <FindState /> */}
     <States/>
+    <Component/>
   </React.StrictMode>
 );
