a2f56a6b2f0afce7bf9ef0cf4b80cbb7292d9453
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-04 19:19:30 +0500

    furniture figma

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Choose from './Choose';
-// import Header from './components/Header.jsx';
-// import Main from './components/Main.jsx';
-// import Navbar from './components/Navbar.jsx';
+import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
+import Navbar from './components/Navbar.jsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
-    {/* <Navbar />
+    <Navbar />
     <Header />
-    <Main/> */}
+    <Main/>
     <Choose />
   </React.StrictMode>
 );
