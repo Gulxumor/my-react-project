8e72031ae93e9b42ba6c0fea570b4218ca5fa8e7
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-07 11:16:42 +0500

    all of sections are finished

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slider from "react-slick";
-import {  Div, Image, Text, Title } from './TestimonialStyle';
+import { Container, Div, Image, Text, Title, } from './TestimonialStyle';
 
 class Testimonials extends React.Component {
     constructor(props) {
@@ -12,73 +12,72 @@ class Testimonials extends React.Component {
         var settings = {
             dots: true,
             infinite: true,
-            speed: 500,
+            speed: 1000,
+            autoplay: true,
             slidesToShow: 1,
             slidesToScroll: 1
         };
         return (
-            <Slider {...settings} >
-                <Div>
-                    <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
-                    <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
-                    <Image />
-                    <Title>Michelle Anna</Title>
-                    <Title w="165" h="32" fs="13" mt="4" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
-                </Div>
-                <Div>
-                    <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
-                    <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
-                    <Image />
-                    <Title>Michelle Anna</Title>
-                    <Title w="165" h="32" fs="13" mt="4" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
-                </Div>
-                <Div>
-                    <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
-                    <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
-                    <Image />
-                    <Title>Michelle Anna</Title>
-                    <Title w="165" h="32" fs="13" mt="4" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
-                </Div>
-                <Div>
-                    <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
-                    <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
-                    <Image />
-                    <Title>Michelle Anna</Title>
-                    <Title w="165" h="32" fs="13" mt="4" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
-                </Div>
-
-                <Div>
-                    <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
-                    <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
-                    <Image />
-                    <Title>Michelle Anna</Title>
-                    <Title w="165" h="32" fs="13" mt="4" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
-                </Div>
-
-                <Div>
-                    <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
-                    <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
-                    <Image />
-                    <Title>Michelle Anna</Title>
-                    <Title w="165" h="32" fs="13" mt="4" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
-                </Div>
-
-                <Div>
-                    <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
-                    <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
-                    <Image />
-                    <Title>Michelle Anna</Title>
-                    <Title w="165" h="32" fs="13" mt="4" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
-                </Div>
-
-                <Div>
-                    <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
-                    <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
-                    <Image />
-                    <Title>Michelle Anna</Title>
-                    <Title w="165" h="32" fs="13" mt="4" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
-                </Div>
-            </Slider>
+            <Container>
+                <Slider {...settings} className="slider">
+                        <Div>
+                            <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
+                            <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
+                            <Image />
+                            <Title>Michelle Anna</Title>
+                            <Title w="170" h="32" fs="13" mt="8" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
+                        </Div>
+                        <Div>
+                            <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
+                            <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
+                            <Image />
+                            <Title>Michelle Anna</Title>
+                            <Title w="170" h="32" fs="13" mt="8" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
+                        </Div>
+                        <Div>
+                            <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
+                            <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
+                            <Image />
+                            <Title>Michelle Anna</Title>
+                            <Title w="170" h="32" fs="13" mt="8" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
+                        </Div>
+                        <Div>
+                            <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
+                            <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
+                            <Image />
+                            <Title>Michelle Anna</Title>
+                            <Title w="170" h="32" fs="13" mt="8" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
+                        </Div>
+                        <Div>
+                            <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
+                            <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
+                            <Image />
+                            <Title>Michelle Anna</Title>
+                            <Title w="170" h="32" fs="13" mt="8" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
+                        </Div>
+                        <Div>
+                            <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
+                            <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
+                            <Image />
+                            <Title>Michelle Anna</Title>
+                            <Title w="170" h="32" fs="13" mt="8" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
+                        </Div>
+                        <Div>
+                            <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
+                            <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
+                            <Image />
+                            <Title>Michelle Anna</Title>
+                            <Title w="170" h="32" fs="13" mt="8" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
+                        </Div>
+                        <Div>
+                            <Title w="209" h="41" fw="600" fs="34" lh="41">Testimonials</Title>
+                            <Text>‘’Donec nibh magna, interdum quis massa sed, rhoncus laoreet quam. Mauris accumsan felis fermentum euismod egestas. Mauris ante augue, cursus sit amet arcu a, maximus suscipit nibh. Integer vel nibh tellus. Pellentesque in risus non dui venenatis sollicitudin sed vitae diam. Fusce tincidunt nisl mi, at molestie odio accumsan non. Pellentesque ma’’</Text>
+                            <Image />
+                            <Title>Michelle Anna</Title>
+                            <Title w="170" h="32" fs="13" mt="8" mb="71">CEO, Co-Founder, XYZ Inc.</Title>
+                        </Div>
+                    </Slider>
+            </Container>
         );
     }
 }
