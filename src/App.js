74d2e5ce6cf738d3e12cb9d04c2a0fe9d87b9450
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-10 15:55:45 +0500

    to-do list created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoList from './components/To do/TodoList'
+
+export default function App() {
+    return (
+        <div className='todo-app'>
+            <TodoList />
+        </div>
+    )
+}
