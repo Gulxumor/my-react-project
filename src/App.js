956e9b9cff6b541d0f1a074ccfba8376c8c979b6
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-03-11 11:46:38 +0500

    media added for max-width:800px phones

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TodoList from './components/To do/TodoList'
 export default function App() {
     return (
         <div className='todo-app'>
+            <a href='https://github.com/Gulxumor/my-react-project/tree/for/src'>Source code</a>
             <TodoList />
         </div>
     )
