21ca05fe2ffbd62bf3c638658d92ea6b13414202
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-04-01 01:43:08 +0500

    react-router0dom

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Root = () => {
   return (
-    <div>Root</div>
+    <div>
+      <Navbar/>
+    </div>
   )
 }
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import "./style.css";
+import { Outlet, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <nav>
+        <h1>Logo</h1>
+        <div>
+          <ul>
+            <li>
+              <NavLink
+                to={"/"}
+                style={({ isActive }) => ({ color: isActive ? "red" : "#fff" })}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={({ isActive }) => ({ color: isActive ? "red" : "#fff" })}
+                to={"/about"}
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={({ isActive }) => ({ color: isActive ? "red" : "#fff" })}
+                to={"/contact"}
+              >
+                Contact
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={({ isActive }) => ({ color: isActive ? "red" : "#fff" })}
+                to={"/main"}
+              >
+                Main
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <button>Login</button>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
 
 export default Navbar
