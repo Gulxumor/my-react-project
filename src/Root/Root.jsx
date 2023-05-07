43518fef0388e5eed162c334c7b12ff7b9e6f9d3
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-05-07 07:49:41 +0500

    useQuery

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Posts from "../components/Posts";
+import Products from "../components/Products";
 
 const Root = () => {
   return (
-    <div>Root</div>
-  )
-}
+    <div>
+      <Products />
+      <Posts />
+    </div>
+  );
+};
 
 export default Root
